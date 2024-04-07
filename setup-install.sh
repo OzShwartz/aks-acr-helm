79d250db68d8628c1b53dc8f84ff863cb526ecc0
Oz Shwartz <ozshwartz@Ozs-MacBook-Pro.local>
2024-04-07 14:38:35 +0300

    update setup-install and create-aks-acr

---
+++ setup-install.sh
@@ -0,0 +1,17 @@
+
+#install kubectl:
+brew install kubectl
+
+#install azure cli:
+brew update && brew install azure-cli
+
+# install helm:
+brew install helm
+
+# create helm chart:
+helm crate my-ask-charts
+
+
+# login to azure account:
+zr login --allow-no-subscriptions
+
